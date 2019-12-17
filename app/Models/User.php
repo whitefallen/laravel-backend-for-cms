5d76e49378dc2bf5c988f1c70f963a98b481b55c
Jonas Baur <jonasbaur@hotmail.de>
2019-12-17 13:18:01 +0100

    for thomy

---
+++ app/Models/User.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Notifications\Notifiable;
+use IlluminateDatabaseEloquentModel;
+
+class User extends Model
+{
+
+}
