7eac1f77405ab2d5c81ba1235e480140aa4f5370
Whitefallen <thomas.kammann@study.hs-duesseldorf.de>
2019-12-17 18:47:45 +0100

    Update CI

---
+++ ci/docker_install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# We need to install dependencies only for Docker
+[[ ! -e /.dockerenv ]] && exit 0
+
+set -xe
+
+# Install git (the php image doesn't have it) which is required by composer
+apt-get update -yqq
+apt-get install git -yqq zlib1g-dev
+
+# Install mysql driver
+# Here you can install any other extension that you need
+docker-php-ext-install pdo_mysql zip
+
+# Install composer
+curl -sS https://getcomposer.org/installer | php
+
+# Install all project dependencies
+php composer.phar install --no-progress
