3dc86fb26bc90de1371e1ec3d3d9f34649f53f79
Whitefallen <thomas.kammann@study.hs-duesseldorf.de>
2020-01-23 13:44:17 +0100

    Webhook stuff

---
+++ database/migrations/2020_01_23_133420_create_webhooks_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWebhooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('webhooks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('url');
+            $table->string('event');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('webhooks');
+    }
+}
